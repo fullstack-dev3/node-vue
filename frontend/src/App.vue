de2955b4c5c6b8078b43024bd215b9b748dfc628
full-stack-david <davidchan12323@gmail.com>
2023-08-19 03:42:26 -0500

    Init Vue3 frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">Home</router-link>
+    </nav>
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
